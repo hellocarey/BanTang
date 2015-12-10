2f38da08044d3e13013e923fb78b524c769f7c60
hellocarey <gdcsj1226@qq.com>
2015-12-10 16:59:37 +0800

    高仿半糖

---
+++ BanTang/Main/View/BTTabbar.h
@@ -0,0 +1,13 @@
+//
+//  BTTabbar.h
+//  BanTang
+//
+//  Created by User on 15/12/10.
+//  Copyright © 2015年 LJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BTTabbar : NSObject
+
+@end
---
+++ BanTang/Main/View/BTTabbar.m
@@ -0,0 +1,13 @@
+//
+//  BTTabbar.m
+//  BanTang
+//
+//  Created by User on 15/12/10.
+//  Copyright © 2015年 LJ. All rights reserved.
+//
+
+#import "BTTabbar.h"
+
+@implementation BTTabbar
+
+@end
